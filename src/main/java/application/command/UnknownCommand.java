4396eff47c2a4e08aded4489725216a9ea87e3c6
Ianos828 <kokbowen17@gmail.com>
2026-03-29 20:26:41 +0800

    Add login command functionality
    Changed abstract command class method signature to include
    authmanager.

--- src/main/java/application/command/UnknownCommand.java
+++ src/main/java/application/command/UnknownCommand.java
@@ -1,5 +1,6 @@
 package application.command;
 
+import application.auth.AuthManager;
 import application.review.ReviewList;
 import application.storage.Storage;
 
@@ -12,10 +13,15 @@ public class UnknownCommand extends Command {
      *
      * @param reviews the list of reviews
      * @param storage the storage object
+     * @param manager the authentication manager
      * @return a string indicating that the command is unknown
      */
     @Override
-    public String execute(ReviewList reviews, Storage storage) {
+    public String execute(
+            ReviewList reviews,
+            Storage storage,
+            AuthManager manager
+    ) {
         return "I'm sorry, I don't understand that command.";
     }
 }
